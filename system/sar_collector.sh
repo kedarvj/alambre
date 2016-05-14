4d9f729d9645728ed805deafd1bd3098ba3d165b
Kedar <k.vaijanapurkar@gmail.com>
2016-05-14 11:33:44 +0530

    fixed by adding required new line before loop

--- system/sar_collector.sh
+++ system/sar_collector.sh
@@ -2,8 +2,9 @@
 
 # Sar to be processed in ksar
 
-DT=$(ls /var/log/sa/sa[0-9][0-9] | tr '\n' ' ' | sed 's/\/var\/log\/sa\/sa/ /g') 
- > /tmp/sar-$(hostname)-multiple.txt for i in $DT; do 
+DT=$(ls /var/log/sa/sa[0-9][0-9] | tr '\n' ' ' | sed 's/\/var\/log\/sa\/sa/ /g')  > /tmp/sar-$(hostname)-multiple.txt
+ 
+for i in $DT; do 
   LC_ALL=C sar -A -f /var/log/sa/sa$i >> /tmp/sar-$(hostname)-multiple.txt 
 done
 
